5785f5889baf1df96ce1d2c1b1fd3e9db815df3c
Adarsh Raj Gautam <arg.adarsh@gmail.com>
2023-06-19 12:31:18 +0530

    starting

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAS4ApTmg-RrbhFKP9y29yLkBEPO3q6kA",
+    authDomain: "netflix-clone-f9919.firebaseapp.com",
+    projectId: "netflix-clone-f9919",
+    storageBucket: "netflix-clone-f9919.appspot.com",
+    messagingSenderId: "1031222504404",
+    appId: "1:1031222504404:web:81325558d164b7abfec441"
+};
